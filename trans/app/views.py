a95c4863de66d0862fafcb9724bb05485660646e
전한수 <142530@jnu.ac.kr>
2022-03-25 19:43:05 +0900

    결과창

--- trans/app/views.py
+++ trans/app/views.py
@@ -1,20 +1,36 @@
+# -*- coding: utf-8 -*-
+
 """
 Definition of views.
 """
+
+import os
+import sys
+import urllib.request
 
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 
-
-
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
+
+    chat = '안녕하세요'
+    name = '사용자님'
+
+    return render(request, 'app/index.html',
         {
+            'user_chat':chat,
+            'user_name':name,
         }
     )
+    
+def result(request):
+    result_text = request.GET['totaltext']
+    return render(request, 'app/result.html', 
+        {
+            'total_text': result_text
+        }
+    ) 
 
--- trans/trans/urls.py
+++ trans/trans/urls.py
@@ -8,8 +8,9 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 
-
 urlpatterns = [
     path('', views.home, name='home'),
+    path('result/', views.result, name='result'),
 
 ]
+
